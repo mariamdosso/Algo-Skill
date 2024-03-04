649c34f1346250305474e76c152ef460a86c020d
mariamdosso <dosomarie02@gmail.com>
2024-03-04 22:07:58 +0000

    competence en c

---
+++ language c/hello_world/main.c
@@ -0,0 +1,8 @@
+#include <stRelease 20.03  rev 11983 (2020-03-12 18:24:30)   gcc 8.1.0 Windows/unicode - 64 bitRelease 20.03  rev 11983 (2020-03-12 18:24:30)   gcc 8.1.0 Windows/unicode - 64 bitRelease 20.03  rev 11983 (2020-03-12 18:24:30)   gcc 8.1.0 Windows/unicode - 64 bitdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
